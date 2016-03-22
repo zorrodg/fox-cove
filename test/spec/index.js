c6782dced78e709e93d77e5518348f083fb0c514
Andres Zorro <andres.zorro@zemoga.com>
2016-03-22 06:51:48 -0500

    WIP: Adding support for tests on coffee break

--- test/spec/index.js
+++ test/spec/index.js
@@ -2,6 +2,9 @@
 
 var assert = require('assert');
 var runProcess = require('./../util/run-process');
+var Q = require('q');
+var path = require('path');
+var readFile = Q.denodeify(require('fs').readFile);
 
 describe('Entry command', function () {
   it('shoud output error message when no msg is set', function (done) {
@@ -18,5 +21,20 @@ describe('Entry command', function () {
         assert(new RegExp('Logged: \'This is a new message\'\n').test(output));
       })
       .done(done);
+  });
+});
+
+describe('Coffee / Work options', function() {
+  it('should log ---- COFFEE BREAK ----', function (done) {
+    runProcess('fxc', ['--coffee'])
+      .then(function (output) {
+        assert.equal(output, 'Logged: \'---- COFFEE BREAK ----\'\n');
+        return readFile(path.resolve('./logs/CURRENT'), 'utf-8');
+      })
+      .then(function (data) {
+        data = data.trim().split('\r\n');
+        assert.equal(data[data.length - 1], '  ---- COFFEE BREAK ----');
+      })
+      .done(done); 
   });
 });
